0e12ef2273848db800a88012e632a8e1801ead30
jiansyah <159568656+jiansyah@users.noreply.github.com>
2024-11-11 16:00:39 +0700

    Add files via upload

--- 1.php
+++ 1.php
@@ -1,10 +1,15 @@
 <?php
-$count = 5;
 
-for ($i= 0; $i < $count; $i++){
-    echo str_repeat(".", $count-$i).str_repeat("+", $i+1) . str_repeat(".", $count-$i). "<br>";
-}    
+function ketupat($count){
+    for ($i= 0; $i < $count; $i++){
+        echo str_repeat(".", $count-$i).str_repeat("+", $i+1) . str_repeat(".", $count-$i). "<br>";
+    }    
 
-for ($i= 0; $i < $count; $i++){
-    echo str_repeat(".", $i+1).str_repeat("+", $count-$i). str_repeat(".", $i+1) . "<br>";
+    for ($i= 0; $i < $count; $i++){
+        echo str_repeat(".", $i+2).str_repeat("+", $count-$i-1). str_repeat(".", $i+2) . "<br>";
+    }
 }
+
+ketupat(5);
+
+//Pakai . karna kalau pake spasi dalam string, output di browser jadi tidak bentuk ketupat
