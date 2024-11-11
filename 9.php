50828464e59a6389e22a3a429166adaa463df4dc
jiansyah <159568656+jiansyah@users.noreply.github.com>
2024-11-11 21:52:19 +0700

    Add files via upload

--- 9.php
+++ 9.php
@@ -24,6 +24,7 @@ class Statistika {
     }
 
     public static function median($data) {
+        sort($data);
         $jumlah = count($data);
         $median = ($data[$jumlah/2] + $data[$jumlah/2-1])/2;
         echo $median;
